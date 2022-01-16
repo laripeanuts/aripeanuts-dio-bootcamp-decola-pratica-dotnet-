f362c5bcaa5e26f51bd088dfac2b29b9a3442039
laripeanuts <larissarabelolf@gmail.com>
2022-01-16 16:28:31 -0300

    finalizacao

--- Program.cs
+++ Program.cs
@@ -37,11 +37,46 @@ namespace Revisao
                     case "2":
                         foreach (var a in alunos)
                         {
-                            Console.WriteLine($"Nome: {a.Nome}. Nota: {a.Nota}.");
+                            if (string.IsNullOrEmpty(a.Nome))
+                            {
+                                Console.WriteLine($"Nome: {a.Nome}. Nota: {a.Nota}.");
+                            }
                         }
                         break;
                     case "3":
-                        //TODO
+                        decimal nrNotaTotal = 0;
+                        var nrAlunosTotal = 0;
+
+                        for (int i=0; i < alunos.Length; i++)
+                        {
+                             if (string.IsNullOrEmpty(alunos[i].Nome))
+                            {
+                                nrNotaTotal = nrNotaTotal + alunos[i].Nota;
+                                nrAlunosTotal++;
+                            }
+                        }
+
+                        var mediaGeral = nrNotaTotal / nrAlunosTotal;
+                        Conceito conceitoGeral;
+
+                        if (mediaGeral < 2)
+                        {
+                            conceitoGeral = Conceito.E;
+                        } else if (mediaGeral < 4) {
+                            conceitoGeral = Conceito.D;
+                        } else if (mediaGeral < 6)
+                        {
+                            conceitoGeral = Conceito.C;    
+                        } else if (mediaGeral < 8)
+                        {
+                            conceitoGeral = Conceito.B;
+                        } else if (mediaGeral < 10)
+                        {
+                            conceitoGeral = Conceito.A;
+                        }
+
+                        Console.WriteLine($"Média geral: {mediaGeral}. Conceito: {conceitoGeral}.");
+
                         break;
                     default:
                         throw new ArgumentOutOfRangeException();
